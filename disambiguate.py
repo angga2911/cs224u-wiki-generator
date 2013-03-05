03e35ed02b47bed74039354bfe6398707ac05dbf
Jason Jong <jjong@stanford.edu>
2013-03-05 01:54:03 -0800

    Disambiguate cleanup
    Cleaned up disambiguate function to work more flawlessly. Also created
    function that gets all links of all articles given in a list.

--- disambiguate.py
+++ disambiguate.py
@@ -7,6 +7,7 @@ import os
 import time
 from urllib2 import urlopen, Request, URLError, HTTPError
 import webbrowser
+import re
 
 wikipedia = 'http://en.wikipedia.org/wiki/'
 
@@ -22,9 +23,10 @@ def disambiguate(keyword):
     
     ulist = wiki_page.find(id='mw-content-text')
     primary_link = ulist.p.a
-    primary_link = primary_link['href'].rsplit('/', 1)[1]
     if primary_link != None:
-      to_return.append(primary_link)
+      primary_link = primary_link['href'].rsplit('/', 1)[1]
+      if primary_link != None:
+        to_return.append(primary_link)
     for ulchild in ulist.findAll('ul'):
       for lichild in ulchild.findAll('li'):
         for link in lichild.findAll('a'):
@@ -37,11 +39,31 @@ def disambiguate(keyword):
     return [keyword]
   except URLError, e:
     return [keyword]
+dumper = 'http://en.wikipedia.org/w/index.php?title=Special:Export&action=submit'
+def get_article_links(links):
+  input_to_dump = "%0A".join(links)
+  try:
+    usock = urllib2.Request(dumper+"&pages="+input_to_dump, headers={"User-Agent" : "Magic Browser"})
+    f = urlopen(usock)
+    print 'downloading ' + dumper+"&pages="+input_to_dump
 
+    text = f.read()
+    link_result = re.findall('\[\[(.*?)\]\]', text)
+    return link_result
+  except HTTPError, e:
+    print "HTTP Error:", e.code, input_to_dump
+  except URLError, e:
+    print "URL Error:", e.reason, input_to_dump
+  return []
   
 
-print disambiguate('Michael_Jordan')
-print disambiguate('Cube')
-print disambiguate('Andrew_Ng')
-
+print get_article_links(disambiguate('Michael_Jordan'))
+print get_article_links(disambiguate('Cube'))
+#print get_article_links(disambiguate('Andrew_Ng'))
+#print disambiguate('Andrew_Ng')
+#print disambiguate('Lexicon')
+#print disambiguate('State')
+#print disambiguate('Legion')
+#print disambiguate('Tell')
+#print disambiguate('Well')
 

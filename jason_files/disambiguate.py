46171826bd1fe0cef0b714887e8003db9acd66a8
Jason Jong <jjong@stanford.edu>
2013-03-13 21:56:48 -0700

    Disambiguate only considers similar words from links

--- jason_files/disambiguate.py
+++ jason_files/disambiguate.py
@@ -38,6 +38,7 @@ def disambiguate(keyword):
                   lnk = lnk[1]
                   if lnk != None:
                     to_return.append(lnk)
+      to_return = [w for w in to_return if levenshtein(w,keyword) <0.6]
       return to_return
     except HTMLParseError, e:
       return [keyword]
@@ -64,8 +65,22 @@ def get_article_links(links):
     print "URL Error:", e.reason, input_to_dump
   return []
   
+def levenshtein(s1, s2):
+  l1 = len(s1)
+  l2 = len(s2)
 
-#print get_article_links(disambiguate('Michael_Jordan'))
+  matrix = [range(l1 + 1)] * (l2 + 1)
+  for zz in range(l2 + 1):
+    matrix[zz] = range(zz,zz + l1 + 1)
+  for zz in range(0,l2):
+    for sz in range(0,l1):
+      if s1[sz] == s2[zz]:
+        matrix[zz+1][sz+1] = min(matrix[zz+1][sz] + 1, matrix[zz][sz+1] + 1, matrix[zz][sz])
+      else:
+        matrix[zz+1][sz+1] = min(matrix[zz+1][sz] + 1, matrix[zz][sz+1] + 1, matrix[zz][sz] + 1)
+  return matrix[l2][l1]/float(max(l1,l2))
+
+#print disambiguate('Michael_Jordan') 
 #print get_article_links(disambiguate('Cube'))
 #print get_article_links(disambiguate('Andrew_Ng'))
 #print disambiguate('Andrew_Ng')
